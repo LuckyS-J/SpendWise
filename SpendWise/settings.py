7a86247429f372a615c8e605965ab581c2972b1e
Szymon Jędrzychowski <Szymon54322@gmail.com>
2025-05-23 18:27:18 +0200

    Connected db

--- SpendWise/settings.py
+++ SpendWise/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'SpendWise.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('ENGINE'),
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT')
     }
 }
 
