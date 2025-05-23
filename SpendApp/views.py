b71d956f667f7c8fe127d51cecb99399a214851f
Szymon Jędrzychowski <Szymon54322@gmail.com>
2025-05-23 17:44:48 +0200

    Created home and register html

--- SpendApp/views.py
+++ SpendApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Home(request):
+  return render(request, 'SpendApp/index.html')
+
+def Register(request):
+  return render(request, 'SpendAPp/register.html')
